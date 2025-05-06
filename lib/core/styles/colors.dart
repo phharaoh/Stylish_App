90b95f96d9031cc1c8dd171f0ad402424d25ae8a
AHMED-EWAIDA\Ahmed Ewaida <ahmedewaida234@gmail.com>
2025-05-06 14:36:46 +0300

    create a color file

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFFF83758);
+  static const Color primaryVariant = Color(0xFFFFCCD5);
+  static const Color textFieldBackground = Color(0xFFA8A8A9);
+  static const Color textFieldIcon = Color(0xFF626262);
+  static const Color textFieldLabelText = Color(0xFF676767);
+  static const Color statusOnline = Color(0xFF4392F9);
+  static const Color onPrimaryText = Color(0xFF000000);
+  static const Color onSecondaryText = Color(0xFFA8A8A9);
+  static const Color onSearch = Color(0xFFBBBBBB);
+  static const Color onPrimaryReat = Color(0xFFEDB310);
+  static const Color onSecondaryReat = Color(0xFFA4A9B3);
+  static const Color likeBackground = Color(0xFFD9D9D9);
+}
